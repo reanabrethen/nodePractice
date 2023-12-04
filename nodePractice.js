e7faccfb9cdb15c765b23431eb14d5a7c56231b4
Admin <reanajb@gmail.com>
2023-12-04 12:05:20 -0500

    updated space

--- nodePractice.js
+++ nodePractice.js
@@ -3,10 +3,10 @@ const getInput = require("./get-input.js");
 
 let userFirstName = getInput(1)
 let userLastName = getInput(2)
-let email = getInput(3)
+
 console.log("Hello, " + userFirstName + " " + userLastName) 
 console.log("This is your name capitalized " + userFirstName.toUpperCase() + " " + userLastName.toUpperCase())
-console.log("These are your initials " + userFirstName[0] + "." + userLastName[0] + ".")
+console.log("These are your initials " + userFirstName[0] + ". " + userLastName[0] + ".")
 console.log("Your email address is " + userFirstName[0].toLowerCase() + userLastName.toLowerCase() + ".prsvr@gmail.com")
 
  
