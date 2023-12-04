4fb38f78e15004caf0a29ac3d11c27aaabd7d3af
Admin <reanajb@gmail.com>
2023-12-04 11:25:13 -0500

    initial commit

---
+++ nodePractice.js
@@ -0,0 +1,10 @@
+// Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
+const getInput = require("./get-input.js");
+
+let userFirstName = getInput(1)
+let userLastName = getInput(2)
+let age = getInput(3)
+console.log(userFirstName[0]) 
+    
+    
+    // + " " + userLastName, age)
